0a9b1918e59d5dd0cf745efe98d61dae2c541fc4
Ruslan Negoda <iarjuna108@gmail.com>
2022-09-03 23:28:22 +0300

    2 task throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,10 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', saveTime);
+player.on('timeupdate', throttle(saveTime, 1000));
 
 function saveTime(evt) {
   let timePlayStop = evt.seconds;
